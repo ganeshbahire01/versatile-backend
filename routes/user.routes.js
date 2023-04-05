fd85756b847599007bf982092d58da7ee1485ac9
somesh9870 <someshrawat106.com>
2023-04-05 13:45:34 +0530

    admin route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ const UserModel = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const userRouter = express.Router();
 const jwt = require("jsonwebtoken");
+const adminauth = require("../middlewares/adminauth.middleware");
 
 userRouter.post("/register", async (req, res) => {
   const { firstname, lastname, email, password, mobile, course, score } =
@@ -39,18 +40,18 @@ userRouter.post("/login", async (req, res) => {
     const user = await UserModel.find({ email: email });
     if (user.length > 0) {
       bcrypt.compare(password, user[0].password, (err, result) => {
-//         if(user[0].isAdmin&&result){
-//              res.status(200).send({
-//             message: "Login successful",
-//             token: (token = jwt.sign({ userID: user[0]._id }, "somesh")),
-//             owner:"edith"
-//           });
-//         }else
+        //         if(user[0].isAdmin&&result){
+        //              res.status(200).send({
+        //             message: "Login successful",
+        //             token: (token = jwt.sign({ userID: user[0]._id }, "somesh")),
+        //             owner:"edith"
+        //           });
+        //         }else
         if (result) {
           res.status(200).send({
             message: "Login successful",
             token: (token = jwt.sign({ userID: user[0]._id }, "somesh")),
-            owner: user[0]
+            owner: user[0],
           });
         } else {
           res.status(400).send({ message: "Invalid password" });
@@ -63,25 +64,41 @@ userRouter.post("/login", async (req, res) => {
     res.status(400).send({ message: err.message });
   }
 });
+
+
 // GET ALL USERS
 userRouter.get("/allusers", async (req, res) => {
   try {
     let users = await UserModel.find({});
-    res.send(users);
+    res.status(200).send(users);
   } catch (error) {
-    res.send(error.message);
+    res.status(400).send(error.message);
   }
 });
 
-// DELETE User
+// DELETE User only by admin
 
 userRouter.delete("/delete/:id", async (req, res) => {
   try {
     let user = await UserModel.findByIdAndDelete(req.params.id);
-    res.send("Delete Successful");
+    res.status(200).send("Delete Successful");
   } catch (error) {
-    res.send(error.message);
+    res.status(400).send(error.message);
   }
 });
-// UPDATE USER
+
+// To approve the user account patching the isLogin status (admin authorization)
+
+userRouter.patch("/approve/:id", adminauth, async (req, res) => {
+  try {
+    let user = await UserModel.findByIdAndUpdate(req.params.id, {
+      isLogin: true,
+    });
+    res.status(200).send("Update Successful");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
 module.exports = userRouter;
+
